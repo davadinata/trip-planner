fefd85a2a20ad5023289b8a03a299aa4750c9fe6
davadinata <theodoxa.dava@gmail.com>
2026-03-01 20:33:28 +0700

    add __init __

--- app/modules/travel-plan/schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class QueriesSchema(BaseModel): 
-    query: list[str] =  Field(description="List of queries to generate travel plan")
-    
-class TravelPlan(BaseModel):
-    plan : str 
